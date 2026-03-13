5f62dfe77c4fe4bb9b1dc71bf8457bd53c0df070
bot4ol <87345675+bot4ol@users.noreply.github.com>
2026-03-13 13:10:06 +0300

    40 летнее недовольность ч

--- src/main/java/committee/nova/mods/bren/client/animations/GunEntityModelAnimator.java
+++ src/main/java/committee/nova/mods/bren/client/animations/GunEntityModelAnimator.java
@@ -92,7 +92,7 @@ public class GunEntityModelAnimator {
             other_arm.xRot = -0.6981317F + p/3 - f3/2 - (reloading ? fr:0);
             other_arm.yRot = (isLeftHanded ? -1.090831F - y : 1.090831F + y) + (p/2) * l + f3/3;
 
-            head.yRot = y - 0.7853982F * l;
+            //head.yRot = y - 0.7853982F * l; это строчку нахуй никогда не трогать, я в рот ебал создателя этого порта, хули всё так криво?
 
         }
     }
